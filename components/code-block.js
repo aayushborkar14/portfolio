504ff346dcb58259c76e7a84ca65a97c0c5056cc
Aayush Borkar <aayushb14@gmail.com>
2023-12-15 22:55:33 +0530

    Add ul and ol support

--- components/code-block.js
+++ components/code-block.js
@@ -3,7 +3,6 @@ import hljs from 'highlight.js'
 import 'highlight.js/styles/atom-one-dark.css'
 
 export default function CodeBlock({ code }) {
-  console.log(hljs.highlightAuto(code.join('')).value)
   return (
     <div className="w-full bg-gray-800 rounded-md p-4">
       <code className="whitespace-pre" dangerouslySetInnerHTML={{ __html: hljs.highlightAuto(code.join('')).value }} />
--- components/post-content.js
+++ components/post-content.js
@@ -19,10 +19,6 @@ export default function PostContent({ postContent }) {
       if (obj.code) {
         modifiedText = (<span key={index} className="font-mono text-pink-600 bg-gray-800 rounded-sm p-1 text-sm">{modifiedText}</span>)
       }
-      if (obj.type == 'link') {
-        const text = obj.children.map(ob => ob.text).join(' ')
-        modifiedText = (<Link key={index} href={obj.href} className="underline">{text}</Link>)
-      }
     }
 
     switch (type) {
@@ -42,6 +38,16 @@ export default function PostContent({ postContent }) {
         return <h6 key={index} className="text-lg font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h6>
       case 'code-block':
         return <div className="mb-8"><CodeBlock code={modifiedText} /></div>
+      case 'numbered-list':
+        return <ol key={index} className="text-lg mb-4 list-decimal list-inside">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</ol>
+      case 'bulleted-list':
+        return <ul key={index} className="text-lg mb-4 list-disc list-inside">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</ul>
+      case 'list-item':
+        return <li key={index}>{obj.children.map((item, i) => <React.Fragment key={i}>{getContentFragment(i, item.text, item, item.type)}</React.Fragment>)}</li>
+      case 'list-item-child':
+        return obj.children.map((item, i) => <React.Fragment key={i}>{getContentFragment(i, item.text, item, item.type)}</React.Fragment>)
+      case 'link':
+        return <Link key={index} href={obj.href} className="underline">{obj.children.map((item, itemIndex) => <React.Fragment key={itemIndex}> {getContentFragment(itemIndex, item.text, item, item.type)}</React.Fragment>)}</Link>
       case 'image':
         return (
           <img
@@ -61,7 +67,7 @@ export default function PostContent({ postContent }) {
   return (
     <div className="w-full">
       {postContent.map((typeObj, index) => {
-        const children = typeObj.children.map((item, itemIndex) => getContentFragment(itemIndex, item.text, item))
+        const children = typeObj.children.map((item, itemIndex) => getContentFragment(itemIndex, item.text, item, item.type))
         return getContentFragment(index, children, typeObj, typeObj.type)
       })}
     </div>
