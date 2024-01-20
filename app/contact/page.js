0d18e6ce57fc7f25c40755c9fe824d9cac764846
Aayush Borkar <aayushb14@gmail.com>
2024-01-20 19:23:02 +0530

    Complete form

--- app/contact/page.js
+++ app/contact/page.js
@@ -8,9 +8,8 @@ export const metadata = {
 export default async function ContactPage() {
   return (
     <div className="container py-10 flex flex-col gap-y-4 md:gap-y-6">
-      <div className="text-2xl md:text-4xl font-bold">Contact me</div>
-      <hr className="bg-muted-foreground border-0 h-px" />
-      <div className="md:px-56"><ContactForm /></div>
+      <div className="text-2xl md:text-4xl font-bold">Get in touch</div>
+      <div className="md:pr-96"><ContactForm /></div>
     </div>)
 }
 
--- components/contact-form.js
+++ components/contact-form.js
@@ -12,6 +12,20 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "./ui/textarea"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import * as React from "react"
+import Link from "next/link"
+import { siteConfig } from "@/config/site"
 
 const formSchema = z.object({
   name: z.string().min(2, { message: "Name must be at least 2 characters." }).max(50, { message: "Character limit hit" }),
@@ -21,11 +35,14 @@ const formSchema = z.object({
 })
 
 export function ContactForm() {
+  const [openOK, setOpenOK] = React.useState(false)
+  const [openER, setOpenER] = React.useState(false)
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
       email: "",
+      subject: "",
       message: "",
     },
   })
@@ -37,46 +54,73 @@ export function ContactForm() {
       },
       method: "POST",
     })
-    if (res.ok) { }
-    else { }
+    if (res.ok) {
+      setOpenOK(true)
+      form.reset()
+    }
+    else { setOpenER(true) }
   }
   return (
-    <Form>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-        <FormField control={form.control} name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Input placeholder="Full name" {...field} />
-              </FormControl>
-            </FormItem>
-          )} />
-        <FormField control={form.control} name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Input placeholder="Email" {...field} />
-              </FormControl>
-            </FormItem>
-          )} />
-        <FormField control={form.control} name="subject"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Input placeholder="Subject" {...field} />
-              </FormControl>
-            </FormItem>
-          )} />
-        <FormField control={form.control} name="message"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Textarea className="resize-none" placeholder="Message" {...field} />
-              </FormControl>
-            </FormItem>
-          )} />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+    <div>
+      <AlertDialog open={openOK} onOpenChange={setOpenOK}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Message sent succesfully</AlertDialogTitle>
+            <AlertDialogDescription>I usually reply within 24 hours.</AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogAction>Close</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+      <AlertDialog open={openER} onOpenChange={setOpenER}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Error encountered</AlertDialogTitle>
+            <AlertDialogDescription>Please try again. If the issue persists, report this issue on <Link className='font-medium' href={`${siteConfig.links.repo}/issues`}>GitHub</Link></AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogAction>Close</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+      <Form>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FormField control={form.control} name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input placeholder="Name" {...field} />
+                </FormControl>
+              </FormItem>
+            )} />
+          <FormField control={form.control} name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input placeholder="Email" {...field} />
+                </FormControl>
+              </FormItem>
+            )} />
+          <FormField control={form.control} name="subject"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input placeholder="Subject" {...field} />
+                </FormControl>
+              </FormItem>
+            )} />
+          <FormField control={form.control} name="message"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Textarea className="resize-none" placeholder="Message" {...field} />
+                </FormControl>
+              </FormItem>
+            )} />
+          <Button type="submit">Send Message</Button>
+        </form>
+      </Form>
+    </div >
   )
 }
