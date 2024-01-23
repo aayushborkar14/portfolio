a3217f7ee2e20b855ea48d4b77975f09be1cd750
Aayush Borkar <aayushb14@gmail.com>
2024-01-23 22:15:08 +0530

    generateStaticParams

--- app/post/[slug]/page.js
+++ app/post/[slug]/page.js
@@ -47,10 +47,10 @@ export default async function PostPage({ params }) {
   )
 }
 
+export async function generateStaticParams() {
+  const posts = await getPosts()
 
-//   const posts = await getPosts()
-//
-//   return posts.map((post) => ({
-//     slug: post.node.slug,
-//   }))
-// }
+  return posts.map((post) => ({
+    slug: post.node.slug,
+  }))
+}
